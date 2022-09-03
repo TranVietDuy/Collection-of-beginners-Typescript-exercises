b1484b1a21d7753fdac933de29dfe3fdd4a2541f
Duy Tran <duytran840@learnit.fi>
2022-09-03 17:42:07 +0300

    finsihed resistor color duo challenge

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,9 @@
+const ColorList = [ 'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white' ];
+
+export function decodedValue(colors: string[]) {
+  if (colors.length < 2) {
+    throw new Error('At least two colors need to be present')
+  }
+  return Number(colors.slice(0, 2).map(color => ColorList.indexOf(color)).join(''))
+}
+    
