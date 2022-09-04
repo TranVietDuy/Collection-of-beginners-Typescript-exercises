5bd23d2b4b8a79432fa326811d031e533e245cce
Duy Tran <duytran840@learnit.fi>
2022-09-04 23:29:29 +0300

    completed

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,6 +1,20 @@
-type planet = 'earth' | 'mercury' | 'venus' | 'mars' | 'jupiter' | 'saturn' | 'uranus' | 'neptune';
-type planetAge = { [key in planet]: number };
-const planetAge: planetAge = {
+// type planet = 'earth' | 'mercury' | 'venus' | 'mars' | 'jupiter' | 'saturn' | 'uranus' | 'neptune';
+// type planetAge = { [key in planet]: number };
+// const planetAge: planetAge = {
+//   earth: 1,
+//   mercury: 0.2408467,
+//   venus: 0.61519726,
+//   mars: 1.8808158,
+//   jupiter: 11.862615,
+//   saturn: 29.447498,
+//   uranus: 84.016846,
+//   neptune: 164.79132,
+// };
+
+// export function age(planet: string, seconds: number): number {
+//  return Math.round((seconds / 31557600 / planetAge[planet as planet]) * 100) / 100;
+// }
+const planetAge: { [planet: string]: number } = {
   earth: 1,
   mercury: 0.2408467,
   venus: 0.61519726,
@@ -9,8 +23,7 @@ const planetAge: planetAge = {
   saturn: 29.447498,
   uranus: 84.016846,
   neptune: 164.79132,
-};
-
+}
 export function age(planet: string, seconds: number): number {
- return Math.round((seconds / 31557600 / planetAge[planet as planet]) * 100) / 100;
+  return Math.round((seconds / 31557600 / planetAge[ planet]) * 100) / 100;
 }
